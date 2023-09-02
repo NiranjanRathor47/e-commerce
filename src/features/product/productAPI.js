a8128c15556e6d73e863d82ef982a58b46bafbcd
niranjan <niranjanrathor888@gmail.com>
2023-09-03 00:23:41 +0530

    part 7  product page filter and change

--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -8,13 +8,22 @@ export function fetchAllProducts() {
   );
 }
  
-export function fetchProductsByFilters(filter) {
-  // filter = {"category":"smartphone"}
+export function fetchProductsByFilters(filter,sort) {
+  // filter = {"category":["smartphone","laptop"]}
+  // sort = {_sort:"price",_order="desc"}
+
+  // TODO : on server we will support multi values
   let queryString = '';
   for(let key in filter){
-    queryString += `${key}=${filter[key]}&`
+    const categoryValues = filter[key];
+    if(categoryValues.length){
+      const lastCatgoryValues = categoryValues[categoryValues.length-1]
+      queryString += `${key}=${lastCatgoryValues}&`
+    }
   }
-  // TODO : we will not hard-code server URL here
+  for(let key in sort){
+    queryString += `${key} = ${sort[key]}&`
+  }
   return new Promise(async (resolve) =>{
    const response = await fetch('http://localhost:5000/products?'+queryString)
    const data = await response.json()
