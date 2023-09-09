7108d26d36dda45818735b3334ef0671368e11df
niranjan <niranjanrathor888@gmail.com>
2023-09-09 19:47:46 +0530

    Part-8  Partition

--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -1,33 +1,39 @@
 export function fetchAllProducts() {
   // TODO : we will not hard-code server URL here
   return new Promise(async (resolve) =>{
-   const response = await fetch('http://localhost:5000/products')
-   const data = await response.json()
-   resolve({data})
+   const response = await fetch('http://localhost:5000/products');
+   const data = await response.json();
+   resolve({data});
 }
   );
 }
  
-export function fetchProductsByFilters(filter,sort) {
+export function fetchProductsByFilters(filter,sort,pagination) {
   // filter = {"category":["smartphone","laptop"]}
   // sort = {_sort:"price",_order="desc"}
-
-  // TODO : on server we will support multi values
+  // pagination = {_page:1,_limit=10}
+  // TODO : on server we will support multi values in filter
   let queryString = '';
   for(let key in filter){
     const categoryValues = filter[key];
     if(categoryValues.length){
-      const lastCatgoryValues = categoryValues[categoryValues.length-1]
-      queryString += `${key}=${lastCatgoryValues}&`
+      const lastCategoryValues = categoryValues[categoryValues.length-1]
+      queryString += `${key}=${lastCategoryValues}&`
     }
   }
-  for(let key in sort){
-    queryString += `${key} = ${sort[key]}&`
+  for (let key in sort) {
+    queryString += `${key}=${sort[key]}&`;
   }
+  for (let key in pagination) {
+    queryString += `${key}=${pagination[key]}&`;
+  }
+
   return new Promise(async (resolve) =>{
-   const response = await fetch('http://localhost:5000/products?'+queryString)
-   const data = await response.json()
-   resolve({data})
+    // TODO we will not hard code server url here
+   const response = await fetch('http://localhost:5000/products?'+queryString);
+   const data = await response.json();
+   const totalItems = await response.headers.get('X-Total-Count');
+   resolve({ data: { products: data, totalItems: +totalItems } });
 }
   );
 }
